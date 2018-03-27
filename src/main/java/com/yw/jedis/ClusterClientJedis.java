a72c123a331451cd52c7305b124d6753ae4f9352
freesky <topsword@163.com>
2018-03-27 09:39:05 +0800

    更新集群代码

--- src/main/java/com/yw/jedis/ClusterClientJedis.java
+++ src/main/java/com/yw/jedis/ClusterClientJedis.java
@@ -1,7 +1,40 @@
 package com.yw.jedis;
 
+import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
+ * 后台集群cluster
  * Created by yw on 2018/3/23.
  */
+@Slf4j
 public class ClusterClientJedis {
+
+    private static JedisCluster jedisCluster;
+
+    static{
+        Set<HostAndPort> jedisClusterNodes  = new HashSet<>();
+        jedisClusterNodes.add(new HostAndPort("192.168.1.201",6379));
+        jedisClusterNodes.add(new HostAndPort("192.168.1.202",6380));
+        jedisClusterNodes.add(new HostAndPort("192.168.1.203",6381));
+         jedisCluster = new JedisCluster(jedisClusterNodes);
+    }
+
+
+    private static void TestString(){
+        log.debug("开始测试String");
+        jedisCluster.set("java","hello world!");
+        String val = jedisCluster.get("java");
+        log.debug("当前key:java的val:{}",val);
+
+    }
+
+    public static void main(String[] args) {
+        TestString();
+    }
+
 }
